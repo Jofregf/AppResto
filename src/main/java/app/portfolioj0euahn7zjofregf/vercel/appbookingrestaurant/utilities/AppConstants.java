66a09fc2aa316ac7fc109ebaf820fbdb824741fb
Guillermo Jofre <jofregf@gmail.com>
2023-06-20 18:43:48 -0300

    logout

--- src/main/java/app/portfolioj0euahn7zjofregf/vercel/appbookingrestaurant/utilities/AppConstants.java
+++ src/main/java/app/portfolioj0euahn7zjofregf/vercel/appbookingrestaurant/utilities/AppConstants.java
@@ -3,7 +3,7 @@ package app.portfolioj0euahn7zjofregf.vercel.appbookingrestaurant.utilities;
 public class AppConstants {
 
     public static final String DEFAULT_NUMBER_PAGE = "0";
-    public static final String DEFAULT_PAGE_SIZE = "9";
+    public static final String DEFAULT_PAGE_SIZE = "6";
     public static final String DEFAULT_SORT_BY = "restaurantName";
     public static final String DEFAULT_SORT_BY_DIRECTION = "asc";
 
